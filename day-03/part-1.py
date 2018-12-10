a077f412b2033c81d17ef46d4999048ca351f773
Gabriel-David ROȘU (87840) <gabriel_david.rosu@stud.acs.upb.ro>
2018-12-10 13:32:43 +0200

    day 3 update

--- day-03/part-1.py
+++ day-03/part-1.py
@@ -1,35 +1,35 @@
+from sys import stdin
+
+
 zones = dict()
 
-with open('input.txt') as file:
-	for line in file:
-		[_, _, startToken, dimToken] = line.split(' ')
+for line in stdin.readlines():
+	[_, _, startToken, dimToken] = line.split(' ')
+	
+	[startW, startH] = startToken.split(',')
+	startW = int(startW)
+	startH = int(startH[:-1])
+	
+	[dimW, dimH] = dimToken.split('x')
+
+	if dimH[-1] == '\n':
+		dimH = dimH[:-1]
+
+	dimW = int(dimW)
+	dimH = int(dimH)
+
+	for i in range(startH, startH + dimH):
+		for j in range(startW, startW + dimW):
+			key = tuple([i, j])
+
+			if key in zones:
+				zones.update({(key, zones[key] + 1)})
+			else:
+				zones.update({(key, 1)})
+
+count = 0
+for value in zones.values():
+	if value >= 2:
+		count += 1
 		
-		[startW, startH] = startToken.split(',')
-		startW = int(startW)
-		startH = int(startH[:-1])
-		
-		[dimW, dimH] = dimToken.split('x')
-
-		if dimH[-1] == '\n':
-			dimH = dimH[:-1]
-
-		dimW = int(dimW)
-		dimH = int(dimH)
-
-		for i in range(startH, startH + dimH):
-			for j in range(startW, startW + dimW):
-				key = tuple([i, j])
-
-				if key in zones:
-					zones.update({(key, zones[key] + 1)})
-				else:
-					zones.update({(key, 1)})
-
-	count = 0
-	for value in zones.values():
-		if value >= 2:
-			count += 1
-			
-	print(count)
-
-
+print(count)
